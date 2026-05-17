25efb11daa9f4388cf0eda3a95eda660c92f3a56
tushig tse <tsetushig@gmail.com>
2026-05-17 06:15:40 +0000

    use this for fix the error (Auth error firebase:error (auth/api-key-not-

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
-
 export const firebaseConfig = {
-  apiKey: "AIzaSy...", // This is a placeholder; Studio handles real config injection
-  authDomain: "project-id.firebaseapp.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef"
+  apiKey: "AIzaSyD6lDik70MEC8LebOSt4hu4ezEoGMxa12c",
+  authDomain: "studio-3372925881-22d73.firebaseapp.com",
+  projectId: "studio-3372925881-22d73",
+  storageBucket: "studio-3372925881-22d73.firebasestorage.app",
+  messagingSenderId: "850688781849",
+  appId: "1:850688781849:web:905976eb79c850f5440488"
 };
